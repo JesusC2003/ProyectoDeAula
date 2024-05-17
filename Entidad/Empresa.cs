75156fea41e83733268fc39ed22a4a1558eaf64b
JesusC2003 <jesusecastillo@unicesar.edu.co>
2024-05-17 15:42:58 -0500

    Creación de entidades

---
+++ Entidad/Empresa.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    public class Empresa
+    {
+        public Empresa()
+        {
+            NIT = string.Empty;
+            Nombre = string.Empty;
+            Telefono = string.Empty;
+            Direccion = string.Empty;
+            Email = string.Empty;
+        }
+
+        public Empresa(string nit, string nombre, string telefono, string direccion, string email)
+        {
+            NIT = nit;
+            Nombre = nombre;
+            Telefono = telefono;
+            Direccion = direccion;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"NIT:{NIT}; NOMBRE:{Nombre}; TELEFONO:{Telefono}; DIRECCION:{Direccion}; EMAIL:{Email}";
+        }
+        
+        public string NIT { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Entidad/PersonalGranja.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    public class PersonalGranja
+    {
+        public PersonalGranja() { }
+        public string Identificaion { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set;}
+
+    }
+}
