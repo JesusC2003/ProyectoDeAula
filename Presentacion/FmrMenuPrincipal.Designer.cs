a7b87c23d6ccf70a737f67797880e76aab731f9e
DiegoCaro19 <dandrescaro@unicesar.edu.co>
2024-05-18 13:47:48 -0500

    Creacion de formularios

---
+++ Presentacion/FmrMenuPrincipal.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Presentacion
+{
+    partial class FmrMenuPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button BtnClose;
+            System.Windows.Forms.Button BtnInventario;
+            System.Windows.Forms.Button BtnAlimento;
+            System.Windows.Forms.Button BtnGalpon;
+            System.Windows.Forms.Button BtnPollo;
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Titulo = new System.Windows.Forms.Label();
+            this.panelbotones = new System.Windows.Forms.Panel();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.panelFmr = new System.Windows.Forms.Panel();
+            BtnClose = new System.Windows.Forms.Button();
+            BtnInventario = new System.Windows.Forms.Button();
+            BtnAlimento = new System.Windows.Forms.Button();
+            BtnGalpon = new System.Windows.Forms.Button();
+            BtnPollo = new System.Windows.Forms.Button();
+            this.panelLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panelbotones.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BtnClose
+            // 
+            BtnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            BtnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            BtnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            BtnClose.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            BtnClose.ForeColor = System.Drawing.Color.White;
+            BtnClose.Image = global::Presentacion.Properties.Resources._4115235_exit_logout_sign_out_114030;
+            BtnClose.Location = new System.Drawing.Point(3, 360);
+            BtnClose.Name = "BtnClose";
+            BtnClose.Size = new System.Drawing.Size(183, 41);
+            BtnClose.TabIndex = 4;
+            BtnClose.UseVisualStyleBackColor = false;
+            // 
+            // BtnInventario
+            // 
+            BtnInventario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            BtnInventario.Cursor = System.Windows.Forms.Cursors.Hand;
+            BtnInventario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            BtnInventario.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            BtnInventario.ForeColor = System.Drawing.Color.White;
+            BtnInventario.Image = global::Presentacion.Properties.Resources.clipboard_note_document_report_paper_icon_192535;
+            BtnInventario.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            BtnInventario.Location = new System.Drawing.Point(1, 222);
+            BtnInventario.Name = "BtnInventario";
+            BtnInventario.Size = new System.Drawing.Size(185, 41);
+            BtnInventario.TabIndex = 3;
+            BtnInventario.Text = "Inventario";
+            BtnInventario.UseVisualStyleBackColor = false;
+            // 
+            // BtnAlimento
+            // 
+            BtnAlimento.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            BtnAlimento.Cursor = System.Windows.Forms.Cursors.Hand;
+            BtnAlimento.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            BtnAlimento.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            BtnAlimento.ForeColor = System.Drawing.Color.White;
+            BtnAlimento.Image = global::Presentacion.Properties.Resources.food_gardening_grading_sack_grain_wheat_icon_255203;
+            BtnAlimento.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            BtnAlimento.Location = new System.Drawing.Point(1, 147);
+            BtnAlimento.Name = "BtnAlimento";
+            BtnAlimento.Size = new System.Drawing.Size(185, 41);
+            BtnAlimento.TabIndex = 2;
+            BtnAlimento.Text = "Alimento";
+            BtnAlimento.UseVisualStyleBackColor = false;
+            // 
+            // BtnGalpon
+            // 
+            BtnGalpon.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            BtnGalpon.Cursor = System.Windows.Forms.Cursors.Hand;
+            BtnGalpon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            BtnGalpon.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            BtnGalpon.ForeColor = System.Drawing.Color.White;
+            BtnGalpon.Image = global::Presentacion.Properties.Resources.house_home_icon_143301;
+            BtnGalpon.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            BtnGalpon.Location = new System.Drawing.Point(1, 74);
+            BtnGalpon.Name = "BtnGalpon";
+            BtnGalpon.Size = new System.Drawing.Size(185, 41);
+            BtnGalpon.TabIndex = 1;
+            BtnGalpon.Text = "Galpon";
+            BtnGalpon.UseVisualStyleBackColor = false;
+            // 
+            // BtnPollo
+            // 
+            BtnPollo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            BtnPollo.Cursor = System.Windows.Forms.Cursors.Hand;
+            BtnPollo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            BtnPollo.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            BtnPollo.ForeColor = System.Drawing.Color.White;
+            BtnPollo.Image = global::Presentacion.Properties.Resources._22266chicken_98785__1_;
+            BtnPollo.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            BtnPollo.Location = new System.Drawing.Point(0, 3);
+            BtnPollo.Name = "BtnPollo";
+            BtnPollo.Size = new System.Drawing.Size(186, 41);
+            BtnPollo.TabIndex = 0;
+            BtnPollo.Text = "Pollo";
+            BtnPollo.UseVisualStyleBackColor = false;
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panelLogo.Controls.Add(this.pictureBox1);
+            this.panelLogo.Location = new System.Drawing.Point(2, 2);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(189, 132);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Presentacion.Properties.Resources.fondo4;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(184, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.Titulo);
+            this.panel1.Location = new System.Drawing.Point(190, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(687, 132);
+            this.panel1.TabIndex = 1;
+            // 
+            // Titulo
+            // 
+            this.Titulo.AutoSize = true;
+            this.Titulo.Font = new System.Drawing.Font("Pristina", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Titulo.ForeColor = System.Drawing.Color.White;
+            this.Titulo.Location = new System.Drawing.Point(122, 4);
+            this.Titulo.Name = "Titulo";
+            this.Titulo.Size = new System.Drawing.Size(421, 126);
+            this.Titulo.TabIndex = 0;
+            this.Titulo.Text = "HENSYS";
+            // 
+            // panelbotones
+            // 
+            this.panelbotones.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.panelbotones.Controls.Add(BtnClose);
+            this.panelbotones.Controls.Add(BtnInventario);
+            this.panelbotones.Controls.Add(BtnAlimento);
+            this.panelbotones.Controls.Add(BtnGalpon);
+            this.panelbotones.Controls.Add(BtnPollo);
+            this.panelbotones.Location = new System.Drawing.Point(3, 136);
+            this.panelbotones.Name = "panelbotones";
+            this.panelbotones.Size = new System.Drawing.Size(188, 404);
+            this.panelbotones.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(0, 0);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(0, 0);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // panelFmr
+            // 
+            this.panelFmr.Location = new System.Drawing.Point(192, 125);
+            this.panelFmr.Name = "panelFmr";
+            this.panelFmr.Size = new System.Drawing.Size(675, 415);
+            this.panelFmr.TabIndex = 3;
+            // 
+            // FmrMenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 539);
+            this.Controls.Add(this.panelFmr);
+            this.Controls.Add(this.panelbotones);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelLogo);
+            this.Name = "FmrMenuPrincipal";
+            this.Text = "FmrMenuPrincipalcs";
+            this.panelLogo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panelbotones.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label Titulo;
+        private System.Windows.Forms.Panel panelbotones;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Panel panelFmr;
+    }
+}
---
+++ Presentacion/FmrMenuPrincipal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentacion
+{
+    public partial class FmrMenuPrincipal : Form
+    {
+        public FmrMenuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private Form activeForm = null;
+        private void openChildForm(Form Fmr)
+        {
+            if (activeForm != null) activeForm.Close();
+            activeForm = Fmr;
+            Fmr.TopLevel = false;
+            Fmr.FormBorderStyle = FormBorderStyle.None;
+            Fmr.Dock = DockStyle.Fill;
+            panelFmr.Controls.Add(Fmr);
+            panelFmr.Tag = Fmr;
+            Fmr.BringToFront();
+            Fmr.Show();
+        }
+    }
+}
