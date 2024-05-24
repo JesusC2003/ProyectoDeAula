cd8470bf2dc7c11bd4c473947f81b316842678f3
DiegoCaro19 <dandrescaro@unicesar.edu.co>
2024-05-24 13:43:39 -0500

    Mejora de menu principal

--- Presentacion/FmrMenuPrincipal.cs
+++ Presentacion/FmrMenuPrincipal.cs
@@ -35,116 +35,184 @@ namespace Presentacion
                 SubMenu.Visible = true;
             }
             else
-                SubMenuInventario.Visible = false;
+                SubMenu.Visible = false;
         }
+        private void PersonalizarSubMenu()
+        {
+            SubMenuAlimento.Visible = false;
+            SubMenuGalpon.Visible = false;
+            PanelSubMenuMedicamento.Visible = false;
+            PanelSubMenuPersonal.Visible = false;
+            PanelSubMenuInventario.Visible = false;
+            panelSubMenuPollo.Visible = false;
+        }
+
         public void OcultarSubMenu()
         {
-            SubMenuInventario.Visible = false;
-            SubMenuPersonal.Visible = false;
+            SubMenuAlimento.Visible = false;
+            SubMenuGalpon.Visible = false;
+            PanelSubMenuMedicamento.Visible = false;
+            PanelSubMenuPersonal.Visible = false;
+            PanelSubMenuInventario.Visible = false;
+            panelSubMenuPollo.Visible = false;
         }
-        public void BtnInventarioP_Click(object sender, EventArgs e)
-        {
-
-            AbrirFmr(new FmrInventarioPollo());
-           
-        }
-
-        public void BtnInventarioG_Click(object sender, EventArgs e)
-        {
-            AbrirFmr(new FmrInventarioGalpon());
-        }
-
-        public void BtnInventarioM_Click(object sender, EventArgs e)
-        {
-            AbrirFmr(new FmrInventarioMedicamento());
-           
-        }
-
-        public void BtnModificalPersonal_Click(object sender, EventArgs e)
-        {
-            AbrirFmr(new FmrPersonalGranja());
-         
-  
-        }
-
-        public void BtnEliminarPersonal_Click(object sender, EventArgs e)
-        {
-            AbrirFmr(new FmrPersonalGranja());
-           
-        }
-
-        public void BtnGuardarPersonal_Click(object sender, EventArgs e)
-        {
-            AbrirFmr(new FmrPersonalGranja());
-          
-        }
-
-        public void BtnPersonal_Click(object sender, EventArgs e)
-        {
-            MostrarSubMenu(SubMenuPersonal);
-        }
-
         public void AbrirFmr(object Fmr)
         {
-            if(this.panelFmr.Controls.Count > 0)
+            if(this.panelContenedor.Controls.Count > 0)
             {
-                this.panelFmr.Controls.RemoveAt(0);
+                this.panelContenedor.Controls.RemoveAt(0);
 
                 Form fm = Fmr as Form;
                 fm.TopLevel = false;
                 fm.Dock = DockStyle.Fill;
-                this.panelFmr.Controls.Add(fm);
-                this.panelFmr.Tag = fm;
+                this.panelContenedor.Controls.Add(fm);
+                this.panelContenedor.Tag = fm;
                 fm.Show();
+                Titulo.Visible=false;
+                labelHora.Visible=false;
+                labelFecha.Visible=false;
             }
         }
 
-        public void BtnPollo_Click(object sender, EventArgs e)
-        {
-            AbrirFmr(new FmrPollo());
-        }
-
-        private void BtnGalpon_Click(object sender, EventArgs e)
-        {
-            AbrirFmr(new FmrGalpon());
-        }
-
-        private void BtnAlimento_Click(object sender, EventArgs e)
-        {
-            AbrirFmr(new FmrAlimento());
-        }
-
-        private void BtnMedicamento_Click(object sender, EventArgs e)
-        {
-            AbrirFmr(new FmrMedicamento());
-        }
-
-        private void BtnDeslizar_Click_1(object sender, EventArgs e)
-        {
-            if (panelbotones.Width == 199)
-            {
-                panelbotones.Width = 46;
-            }
-            else
-            {
-                panelbotones.Width = 199;
-            }
-        }
-
-        private void BtnInventario_Click(object sender, EventArgs e)
-        {
-            MostrarSubMenu(SubMenuInventario);
-        }
-
-        private void BtnClose_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
 
         private void HoraFecha_Tick(object sender, EventArgs e)
         {
             labelHora.Text = DateTime.Now.ToString("hh:mm:ss tt");
             labelFecha.Text = DateTime.Now.ToString("dd/MM/yy");
+        }
+
+        private void BtnPollo_Click(object sender, EventArgs e)
+        {
+            MostrarSubMenu(panelSubMenuPollo);
+        }
+        private void BtnGuardarPollo_Click(object sender, EventArgs e)
+        {
+            AbrirFmr(new FmrPollo());
+            OcultarSubMenu();
+        }
+
+        private void BtnModificalPollo_Click(object sender, EventArgs e)
+        {
+            OcultarSubMenu();
+        }
+
+        private void BtnEliminarPollo_Click(object sender, EventArgs e)
+        {
+            OcultarSubMenu();
+        }
+
+        private void BtnGalpon_Click(object sender, EventArgs e)
+        {
+  
+            MostrarSubMenu(SubMenuGalpon);
+        }
+
+        private void btnGuardarGalpon_Click(object sender, EventArgs e)
+        {
+            AbrirFmr(new FmrGalpon());
+            OcultarSubMenu();
+        }
+
+        private void BtnModificarGalpon_Click(object sender, EventArgs e)
+        {
+            OcultarSubMenu();
+        }
+
+        private void BtnEliminarGalpon_Click(object sender, EventArgs e)
+        {
+            OcultarSubMenu();
+        }
+
+        private void BtnAlimento_Click(object sender, EventArgs e)
+        {
+            MostrarSubMenu(SubMenuAlimento);
+        }
+
+        private void BtnGuardarAlimento_Click(object sender, EventArgs e)
+        {
+            AbrirFmr(new FmrAlimento());
+            OcultarSubMenu();
+        }
+
+        private void BtnModificarAlimento_Click(object sender, EventArgs e)
+        {
+            OcultarSubMenu();
+        }
+
+        private void BtnEliminarAlimento_Click(object sender, EventArgs e)
+        {
+            OcultarSubMenu();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MostrarSubMenu(PanelSubMenuMedicamento);
+        }
+
+        private void BtnGuardarMedicamento_Click(object sender, EventArgs e)
+        {
+            AbrirFmr(new FmrMedicamento());
+
+            OcultarSubMenu();
+        }
+
+        private void BtnModificarMedicamento_Click(object sender, EventArgs e)
+        {
+            OcultarSubMenu();
+        }
+
+        private void BtnEliminarMedicamento_Click(object sender, EventArgs e)
+        {
+            OcultarSubMenu();
+        }
+
+        private void BtnPersonal_Click(object sender, EventArgs e)
+        {
+            MostrarSubMenu(PanelSubMenuPersonal);
+        }
+
+        private void BtnGuardarPersonal_Click(object sender, EventArgs e)
+        {
+            AbrirFmr(new FmrPersonalGranja());
+            OcultarSubMenu();
+        }
+
+        private void BtnModificarPersonal_Click(object sender, EventArgs e)
+        {
+            OcultarSubMenu();
+        }
+
+        private void BtnEliminarPersonal_Click(object sender, EventArgs e)
+        {
+            OcultarSubMenu();
+        }
+
+        private void BtnInventario_Click(object sender, EventArgs e)
+        {
+            MostrarSubMenu(PanelSubMenuInventario);
+        }
+
+        private void BtnInventariopollo_Click(object sender, EventArgs e)
+        {
+            AbrirFmr(new FmrInventarioPollo());
+            OcultarSubMenu();
+        }
+
+        private void BtnInventarioMedicamento_Click(object sender, EventArgs e)
+        {
+            AbrirFmr(new FmrInventarioMedicamento());
+            OcultarSubMenu();
+        }
+
+        private void BtnInventarioGalpon_Click(object sender, EventArgs e)
+        {
+            AbrirFmr(new FmrInventarioGalpon());
+            OcultarSubMenu();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
